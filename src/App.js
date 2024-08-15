c31c792cfa2e73b0a3e560a0c675e7965926db19
Amit <amit.p.patel79@gmail.com>
2024-08-14 22:51:00 -0500

    "second"

--- src/App.js
+++ src/App.js
@@ -13,16 +13,18 @@ function App() {
   const [cart, setCart ] = useState([]);
   
   function addToCart(book) {
-    setCart([...cart,book])
-
+    setCart([...cart, book])
+   
   }
-
-
 
   useEffect(() => {
     console.log(cart)
   }, [cart])
 
+
+
+
+ 
   return (
     <Router>
     
