2a115653fefe9c003664011340d3477e588b4940
Amit <amit.p.patel79@gmail.com>
2024-08-14 20:53:08 -0500

    first int.

---
+++ src/components/Landing.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Undrawbooks from '../assets/Undraw_Books.svg'
+import { Link } from "react-router-dom";
+const Landing = ()=> {
+    return (
+        <section id="Landing">
+            <header>
+                <div className="header__container">
+                    <div className="header__description">
+                        <h1>America's most awarded online library platform</h1>
+                        <h2>Find your dream book with<span className="purple"> library.</span></h2>
+                        <Link to="#features">
+                        <button className="btn">Browse Books</button>
+                        </Link>
+                    </div>
+                    <figure className="header__img--wraper">
+                        <img src= {Undrawbooks} alt="" />
+                    </figure>
+                </div>
+            </header>
+        </section>
+    )
+}
+
+export default Landing;
---
+++ src/components/ui/Rating.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const Rating = ({ rating }) => {
+    return (
+        <div className="book__rating">
+            {<div className="book__ratings">
+        {new Array(Math.floor(rating)).fill(0).map((element, index) => (
+          <FontAwesomeIcon icon="star" key={index} />
+        ))}
+        {Number.isInteger(rating) ? (
+          ""
+        ) : (
+          <FontAwesomeIcon icon="star-half-alt" />
+        )}
+      </div>}       
+        </div>
+
+    );
+}
+
+export default Rating;
